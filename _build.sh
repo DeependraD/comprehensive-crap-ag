b980337d1f4db894c2e174e6210360855d124c9b
DeependraD <dean.ionized@gmail.com>
2020-09-22 17:01:37 +0545

    Cleared all builds inside _book/ and build script improved

--- _build.sh
+++ _build.sh
@@ -2,7 +2,6 @@
 
 set -ev
 
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
-
+Rscript -e "bookdown::render_book('index.Rmd', output_format = 'bookdown::gitbook', new_session = FALSE, config_file = '_bookdown.yml')"
+Rscript -e "bookdown::render_book('index.Rmd', output_format = 'bookdown::pdf_book', new_session = FALSE, config_file = '_bookdown.yml')"
+Rscript -e "bookdown::render_book('index.Rmd', output_format = 'bookdown::epub_book', new_session = FALSE, config_file = '_bookdown.yml')"
